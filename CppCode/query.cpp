187dd3dff4f37c094ba1d05713a33ad5020b36bf
Yasin Alam <yasin.alam@tamu.edu>
2026-05-09 23:56:59 -0500

    added timer

--- CppCode/query.cpp
+++ CppCode/query.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <stdio.h>
 #include "sqlite3.h"
+#include <sys/time.h>
 
 using namespace std;
 
+/* Store all query results here */
+vector<vector<string>> results;
+
+/* Function declarations */
 static int selectData(const char* s);
-static int callback(void* NotUsed, int argc, char** argv, char** azColName);
+static int callback(void* data, int argc, char** argv, char** azColName);
 
 int main()
 {
-    const char* dir = "..\\library.db";
+    ios::sync_with_stdio(false);
+    struct timeval start, stop; 
+    double total_time;
+    
+
+
+    const char* dir =
+        "C:\\Users\\yalam\\Documents\\devWeinbergLedger\\library.db";
+
+    /* Run query and store results */
+    gettimeofday(&start, NULL); 
+
     selectData(dir);
-	
+
+	gettimeofday(&stop, NULL); 
+    total_time = (stop.tv_sec-start.tv_sec)+0.000001*(stop.tv_usec-start.tv_usec);
+    printf("Total Time: %8.4f\n seconds",	 total_time);
+
+    /* Print results AFTER query finishes */
+    cout << "\n===== QUERY RESULTS =====\n" << endl;
+
+    for (const auto& row : results) {
+
+        for (const auto& value : row) {
+            cout << value << " | ";
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
 
@@ -20,7 +54,8 @@ static int selectData(const char* s)
     sqlite3* DB;
     char* messageError;
 
-    string sql = "SELECT * FROM LIBRARY WHERE TITLE LIKE '%FREED%';";
+    string sql =
+        "SELECT * FROM LIBRARY WHERE TITLE LIKE '%FREED%';";
 
     int exit = sqlite3_open(s, &DB);
 
@@ -29,16 +64,26 @@ static int selectData(const char* s)
         return 1;
     }
 
-    exit = sqlite3_exec(DB, sql.c_str(), callback, NULL, &messageError);
+    /* Execute query */
+    exit = sqlite3_exec(
+        DB,
+        sql.c_str(),
+        callback,
+        NULL,
+        &messageError
+    );
 
     if (exit != SQLITE_OK) {
+
         cerr << "SQL Error: " << messageError << endl;
         sqlite3_free(messageError);
-    }
-    else {
-        cout << "Records selected Successfully!" << endl;
+
+    } else {
+
+        cout << "Records fetched successfully.\n";
     }
 
+    /* Close DB */
     exit = sqlite3_close(DB);
 
     if (exit != SQLITE_OK) {
@@ -48,24 +93,26 @@ static int selectData(const char* s)
     return 0;
 }
 
-// retrieve contents of database used by selectData()
-/* argc: holds the number of results, argv: holds each value in array, azColName: holds each column returned in array, */
-static int callback(void* NotUsed, int argc, char** argv, char** azColName)
+/* Callback now STORES rows instead of printing */
+static int callback(
+    void* data,
+    int argc,
+    char** argv,
+    char** azColName
+)
 {
+    vector<string> row;
+
     for (int i = 0; i < argc; i++) {
-        cout << azColName[i] << " = ";
 
         if (argv[i]) {
-            cout << argv[i];
+            row.push_back(argv[i]);
         } else {
-            cout << "NULL";
+            row.push_back("NULL");
         }
-
-        cout << endl;
     }
 
-    cout << "---------------------" << endl;
+    results.push_back(row);
 
-	return 0;
-
+    return 0;
 }
